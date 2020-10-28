ee5117c8afb79cf29a83f18d0ee630bace841d09
weirdflex99 <pradhanpragya2000@gmail.com>
2020-10-28 12:52:55 +0530

    second commit

---
+++ linkedlist/src/linkedlist/Middle.java
@@ -0,0 +1,74 @@
+package linkedlist;
+
+
+
+// Java program to find middle of linked list 
+class Middle
+{ 
+    Node head; 
+  
+    
+    class Node 
+    { 
+        int data; 
+        Node next; 
+        Node(int d) 
+        { 
+            data = d; 
+            next = null; 
+        } 
+    } 
+  
+    /* Function to print middle of linked list */
+    void printMiddle() 
+    { 
+        Node slow_ptr = head; 
+        Node fast_ptr = head; 
+        if (head != null) 
+        { 
+            while (fast_ptr != null && fast_ptr.next != null) 
+            { 
+                fast_ptr = fast_ptr.next.next; 
+                slow_ptr = slow_ptr.next; 
+            } 
+            System.out.println("The middle element is [" + 
+                                slow_ptr.data + "] \n"); 
+        } 
+    } 
+  
+    
+    public void push(int new_data) 
+    { 
+        
+        Node new_node = new Node(new_data); 
+  
+        
+        new_node.next = head; 
+  
+       
+        head = new_node; 
+    } 
+  
+    
+    public void printList() 
+    { 
+        Node tnode = head; 
+        while (tnode != null) 
+        { 
+            System.out.print(tnode.data+"->"); 
+            tnode = tnode.next; 
+        } 
+        System.out.println("NULL"); 
+    } 
+  
+    public static void main(String [] args) 
+    { 
+        Middle llist = new Middle(); 
+        for (int i=1; i<=5; i++) 
+        { 
+            llist.push(i); 
+            llist.printList(); 
+            llist.printMiddle(); 
+        } 
+    } 
+} 
